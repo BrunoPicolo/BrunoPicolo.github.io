65109c02ec11dd8972f2eae379c4590d7db80236
BrunoPicolo <BrunoPicolo@users.noreply.github.com>
2022-09-29 18:05:42 +0000

    deploy: 96009c86b49531abe78ab4b729e837f8750d691a

--- src/app/features/projects-page/models/Project.ts
+++
@@ -1,8 +0,0 @@
-export interface Project {
-  /* Responsibility: Defines project properties */
-  name: string,
-  company: string,
-  image: string,
-  description: string,
-  technologies: string[]
-}
